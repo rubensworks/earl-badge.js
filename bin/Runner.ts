ab548b817a0766e3efea50c7a12fa35b2f5836d9
Ruben Taelman <rubensworks@gmail.com>
2019-10-19 20:54:43 +0200

    Add EARL compliance parser

--- bin/Runner.ts
+++ bin/Runner.ts
@@ -3,22 +3,29 @@
 // tslint:disable:no-console
 // tslint:disable:no-var-requires
 
+import {createReadStream} from "fs";
 import {BadgeGenerator} from "../lib/BadgeGenerator";
+import {EarlParser} from "../lib/EarlParser";
 
 const args = process.argv.slice(2);
 
-if (args.length !== 1) {
+if (args.length !== 3) {
   console.error(`earl-badge creates an SVG badge for a given EARL file
 
 Usage:
-  earl-badge earl-report.ttl
+  earl-badge http://example.org/mysoftwaresubject earl-report.ttl Spec-Name
 `);
   process.exit(1);
 }
 
-const badgeGenerator = new BadgeGenerator();
-const svg = badgeGenerator.createSvgBadge({
-  label: 'JSON-LD',
-  value: '100%',
-});
-console.log(svg);
+(async function run() {
+  const earlParser = new EarlParser();
+  const { compliance } = await earlParser.parse(args[0], createReadStream(args[1]), args[1]);
+
+  const badgeGenerator = new BadgeGenerator();
+  const svg = badgeGenerator.createSvgBadge({
+    label: args[2],
+    value: compliance.toFixed(2) + '%',
+  });
+  console.log(svg);
+})();
---
+++ lib/EarlParser.ts
@@ -0,0 +1,35 @@
+import {RdfObjectLoader, Resource} from "rdf-object";
+import rdfParser from "rdf-parse";
+import {Readable} from "stream";
+
+export class EarlParser {
+
+  public async parse(subject: string, readable: Readable, path: string): Promise<IEarlData> {
+    const objectLoader = new RdfObjectLoader({ context: require('./context-earl.json') });
+    await objectLoader.import(rdfParser.parse(readable, { path , baseIRI: 'http://example.org/' }));
+
+    // Collect all assertions for the given test subject
+    const assertions: Resource[] = [];
+    for (const resource in objectLoader.resources) {
+      if (objectLoader.resources[resource].properties.testSubject
+        .some((earlSubject) => earlSubject.term.value === subject)) {
+        assertions.push(objectLoader.resources[resource]);
+      }
+    }
+
+    // Calculate percentage of succeeded tests
+    const passed = assertions.reduce((sum, assertion) => {
+      if (assertion.property.testResult.property.resultOutcome.value !== 'http://www.w3.org/ns/earl#failed') {
+        return sum + 1;
+      }
+      return sum;
+    }, 0);
+
+    return { compliance: passed * 100 / assertions.length };
+  }
+
+}
+
+export interface IEarlData {
+  compliance: number;
+}
